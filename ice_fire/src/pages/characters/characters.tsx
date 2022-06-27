4370b16d19169b446d401c9a2760d7f07e6d67ee
Ilya Tasukha <pliushkin93@gmail.com>
2022-06-28 00:10:15 +0300

    + modal

--- ice_fire/src/pages/characters/characters.tsx
+++ ice_fire/src/pages/characters/characters.tsx
@@ -3,6 +3,21 @@ import IceandfireApi from "../../servises/iceandfire";
 import { Characters } from "../../types/characters";
 import "./style.css";
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 type TProps = any;
 type TState = {
@@ -11,6 +26,8 @@ type TState = {
   isError: boolean;
   page: number;
   pageSize: number;
+  open: boolean;
+  setOpen: boolean;
 };
 export default class character extends React.Component<TProps, TState> {
   state = {
@@ -18,8 +35,11 @@ export default class character extends React.Component<TProps, TState> {
     isLoading: false,
     isError: false,
     page: 1,
-    pageSize: 20
+    pageSize: 20,
+    open: false,
+    setOpen: true
   };
+  
 
   getCharacters = () => {
     const { page, pageSize } = this.state;
@@ -46,11 +66,15 @@ export default class character extends React.Component<TProps, TState> {
   };
   componentDidUpdate() {
     console.log("componentDidUpdate");
-  }
+  };
 
   render() {
     console.log(this.state);
     const { characters, isLoading, isError } = this.state;
+    const { open, setOpen } = this.state;
+    const handleOpen = () =>  this.setState({ setOpen: true, open: true});
+    const handleClose = () =>  this.setState({ setOpen: false, open: false});
+
     return (
       <div className="characters">
         {isError && "Error"}
@@ -59,9 +83,25 @@ export default class character extends React.Component<TProps, TState> {
           !isError &&
           !isLoading &&
           this.state.characters.map((el: Characters) => (
-            <div key={el.name}>
-              {el.name} {el.gender}
-            </div>
+            <Button key={el.url}>
+              <div>
+                <div onClick={handleOpen}> {el.name} </div>  
+                  <Modal
+                     open={open}onClose={handleClose}aria-labelledby="modal-modal-title"aria-describedby="modal-modal-description">
+                    <Box sx={style}>
+                      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        <div>Name: {el.name}</div> 
+                        <div>Gender: {el.gender}</div>
+                        <div>Culture: {el.culture}</div>
+                        <div>Born: {el.born}</div>
+                        <div>Died: {el.died}</div>
+                        <div>Titles: {el.titles}</div>
+                        <div>Played by: {el.playedBy}</div>                      
+                      </Typography>
+                    </Box>
+                  </Modal>
+              </div>
+            </Button>
           ))}
         {characters && !isError && (
           <div className="characters-nav">
@@ -71,7 +111,8 @@ export default class character extends React.Component<TProps, TState> {
             >
               left
             </Button>
-            <Button variant="outlined" onClick={(_e: any) => this.handlerPage("right")}>
+            <Button variant="outlined" 
+              onClick={(_e: any) => this.handlerPage("right")}>
               right
             </Button>
           </div>
